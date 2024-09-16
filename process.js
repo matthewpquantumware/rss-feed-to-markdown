cf71f024f634c17f69c69ad4c9cd425616903d17
matthewpquantumware <matthewp@quantumware.io>
2024-09-16 01:33:44 -0400

    Update process.js

--- process.js
+++ process.js
@@ -38,8 +38,8 @@ const generateMarkdown = (template, entry) => {
   if (thumbnail == ''){
     try{
       const dom = new JSDOM(content)
-      const thumbnail = dom.window.document.querySelector("img") ; 
-      console.log(`dom '${dom.window.document.querySelector("img").src}' `);
+      const thumbnail = dom.window.document.querySelector("img")?.src?.toString || '' ; 
+      console.log(`dom '${dom.window.document.querySelector("img").src.toString}' `);
     }catch{}
     
   }
