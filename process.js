2b3383bb364f3f20c6129991cdd5c22ff96e2272
matthewpquantumware <matthewp@quantumware.io>
2024-09-16 01:18:35 -0400

    Update process.js

--- process.js
+++ process.js
@@ -29,7 +29,7 @@ const generateMarkdown = (template, entry) => {
   const video = entry['media:group']?.[0]?.['media:content']?.[0]?.$?.url || '';
   const image = entry['media:group']?.[0]?.['media:thumbnail']?.[0]?.$.url || entry['media:thumbnail']?.[0]?.$.url || '';
   const dom = new JSDOM(content)
-  const images = (entry['enclosure'] || entry['media:content'])?.filter(e => imageTypes.includes(e.$['type']))?.map(e => e.$.url) || dom.window.document.querySelectorAll("img")?.[0]?.$.src || [];
+  const images = (entry['enclosure'] || entry['media:content'])?.filter(e => imageTypes.includes(e.$['type']))?.map(e => e.$.url) || dom.window.document.querySelectorAll("img")?.[0]?.src || [];
   const categories = entry.category || [];
   const views = entry['media:group']?.[0]?.['media:community']?.[0]?.['media:statistics']?.[0]?.$.views || '';
   const rating = entry['media:group']?.[0]?.['media:community']?.[0]?.['media:starRating']?.[0]?.$.average || '';
