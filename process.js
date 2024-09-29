b921ff556d319b1ac055d0d5a9ab2b5d54578ece
matthewpquantumware <102489376+matthewpquantumware@users.noreply.github.com>
2024-09-29 16:50:12 -0400

    Update process.js

--- process.js
+++ process.js
@@ -34,7 +34,7 @@ const generateMarkdown = (template, entry) => {
   const rating = entry['media:group']?.[0]?.['media:community']?.[0]?.['media:starRating']?.[0]?.$.average || '';
   const dom = new JSDOM(content)
   const thumbnail = (entry['enclosure'] || entry['media:content'])?.filter(e => imageTypes.includes(e.$['type']))?.map(e => e.$.url) || dom.window.document.querySelector("img").src || '';
-  const textmd =new TurndownService({codeBlockStyle: 'fenced', fenced: '```', bulletListMarker: '-'}).turndown(dom.window.document.querySelector("p"));
+  const textmd =new TurndownService({codeBlockStyle: 'fenced', fenced: '```', bulletListMarker: '-'}).turndown(dom.window.document.querySelector("p").innerhtml);
   
 
 
